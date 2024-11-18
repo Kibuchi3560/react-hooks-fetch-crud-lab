c279a7243015b69cebdfb96392ebe843623cfdeb
Kibuchi3560 <jontezbuchiki3560@gmail.com>
2024-11-18 11:21:37 +0300

    To be Reviewed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function QuestionList() {
+
+  const [questions , setQuestions] = useState([]);
+
+  useEffect(()=> {
+    fetch ("http://localhost:4000/questions")
+    .then ((r) => r.json())
+    .then ((questions) => {console.log(questions)
+      setQuestions(questions)
+    })
+    .catch(error => console.error('Error fetching data:', error))
+  } ,[])
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map ((prompt , id) => (
+        <li key={id}> {prompt} </li>
+      ))}</ul>
     </section>
   );
 }
